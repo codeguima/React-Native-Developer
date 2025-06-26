d6440168be70f53062d04242af75376c6abb9b4d
Jhonny Guimarães <111026602+codeguima@users.noreply.github.com>
2025-06-26 19:31:08 -0300

    ok

---
+++ green-lantern/App.tsx
@@ -0,0 +1,42 @@
+import React, {useState} from 'react';
+import { StyleSheet, Text, View, Image, TouchableOpacity} from 'react-native';
+import symbolOn from "./assets/pictures/symbol-on.png";
+import symbolOff from './assets/pictures/symbol-off.png';
+
+export default function App() {
+  const [isActive, setisActive] = useState(false)
+
+  function handleSymbol(){
+    
+    setisActive((oldValue:boolean) =>{
+      return !oldValue
+    })
+
+    console.log(isActive)
+  }
+
+  return (
+    <View style={isActive ? styles.containerOn : styles.containerOff}>
+      <TouchableOpacity onPress={handleSymbol} >
+          <Image 
+            source={isActive ? symbolOn : symbolOff}
+          />
+      </TouchableOpacity>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  containerOn: {
+    flex: 1,
+    backgroundColor: 'black',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  containerOff:{
+    flex: 1,
+    backgroundColor: '#ffffff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  }
+});
