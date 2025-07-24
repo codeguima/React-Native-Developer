7f7ba5cb0cb61ebb6ba952b0655eb1cd498c7c8d
Jhonny Guimarães <111026602+codeguima@users.noreply.github.com>
2025-07-23 23:02:56 -0300

    ok

--- react-native-cam/App.tsx
+++ react-native-cam/App.tsx
@@ -1,39 +1,30 @@
-import { CameraView, CameraType, useCameraPermissions } from 'expo-camera';
+import { CameraType, useCameraPermissions } from 'expo-camera';
 import { useState } from 'react';
-import { Button, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import { Button, StyleSheet, Text, View } from 'react-native';
+import CustomCameraView from './src/components/CustomCameraView';
 
 export default function App() {
-  const [facing, setFacing] = useState<CameraType>('back');
+  const [type, setType] = useState<CameraType>('back');
   const [permission, requestPermission] = useCameraPermissions();
 
-  if (!permission) {
-    // Camera permissions are still loading.
-    return <View />;
-  }
+  if (!permission) return <View />;
 
   if (!permission.granted) {
-    // Camera permissions are not granted yet.
     return (
       <View style={styles.container}>
         <Text style={styles.message}>Precisamos da sua permissão para mostrar a câmera</Text>
-        <Button onPress={requestPermission} title="conceder permissão" />
+        <Button onPress={requestPermission} title="Conceder permissão" />
       </View>
     );
   }
 
   function toggleCameraFacing() {
-    setFacing(current => (current === 'back' ? 'front' : 'back'));
+    setType(current => (current === 'back' ? 'front' : 'back'));
   }
 
   return (
     <View style={styles.container}>
-      <CameraView style={styles.camera} facing={facing} ratio='4:3'>
-        <View style={styles.mainView}>
-          <TouchableOpacity style={styles.flipArea} onPress={toggleCameraFacing}>
-            <Text style={styles.flipText}>Flip Camera</Text>
-          </TouchableOpacity>
-        </View>
-      </CameraView>
+      <CustomCameraView type={type} onFlipCamera={toggleCameraFacing} />
     </View>
   );
 }
@@ -41,34 +32,9 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
   },
   message: {
     textAlign: 'center',
     paddingBottom: 10,
   },
-  camera: {
-    flex: 1,
-  },
-  mainView: {
-    flex: 1,
-    flexDirection: 'row',
-    backgroundColor: 'transparent',
-    margin: 64,
-  },
-  button: {
-    flex: 1,
-    alignSelf: 'flex-end',
-    alignItems: 'center',
-  },
-  flipText: {
-    fontSize: 20,
-    marginBottom:15,
-    fontWeight: 'bold',
-    color: 'white',
-  },
-  flipArea:{
-    position:'absolute',
-    bottom: 20,
-  }
 });
